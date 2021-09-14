3b6411de4f85c889746f8e11e3315525ab57d04a
Joy <1426075847@qq.com>
2021-09-14 11:58:30 +0800

    master first commit

---
+++ src/main/java/com/oyj/git/test.java
@@ -0,0 +1,11 @@
+package com.oyj.git;
+
+/**
+ * @Author Joy
+ * @Date 2021/9/14 11:56
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
